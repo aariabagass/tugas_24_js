e9d9045966eb30332445643719fd2128a180f1bb
Aria Bagas <aariabagass@gmail.com>
2019-08-23 22:25:20 +0700

    JS Task 24
    array sort reverse

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+var sortRevrs = () => {
+  lineUp = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+  console.log("sebelumnya : " + lineUp);
+  console.log("Ascending : " + lineUp.sort());
+  console.log("Descending : " + lineUp.reverse());
+  return sortRevrs;
+}
+sortRevrs();
